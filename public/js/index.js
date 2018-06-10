5ec272219e66201d72caa9c258098b268fe949ae
tsabunkar <tsabunkar@gmail.com>
2018-06-10 18:27:03 +0530

    Basic UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,35 @@
 var socketClient = io(); //we are making the request from client to server -> to open-up a websocket connection, and always keep it open
 
-//This is to listen to an event , which would be sent from server to client
-//this is connect event , which connects to the server
+//This is to listen to an event , which would be sent from server to client this is connect event , which connects to the server
 socketClient.on('connect', () => { //arrow function is not supported in IE
     console.log('connected to server');
 
-    //creating a custom event and emitting it from client to server
-  /*   socketClient.emit('createEmail', {
-        to:'usha@gmail.com',
-        text : 'Hey this is usha sabunkar'
-    }); */
-
-   /*  socketClient.emit('createMessage',{
-        from:'Shailesh',
-        text : 'Hey this is Shailesh sabunkar'
-    })
- */
-});//end of connect listener
+}); //end of connect listener
 
 //this diconnect event will be fired, when the connection will be dropped/closed
 socketClient.on('disconnect', () => {
     console.log('disconnected from server');
 })
 
-//this is the listener, for newEmail (which is our custome event)
-/* socketClient.on('newEmail', (dataSendFromServer) => {
-    console.log('new email',dataSendFromServer);
-}) */
 
-//chat application
 socketClient.on('newMessage', (dataSendFromServer) => {
-    console.log('Got new Message',dataSendFromServer);
+    console.log('Got new Message', dataSendFromServer);
+    var liElement = $('<li></li>');
+    liElement.text(`${dataSendFromServer.from}: ${dataSendFromServer.text}`);
+    $("#messageOrderListId").append(liElement);
 })
 
+
+//jquery, here we r overridding the default behaviour of page load when <form> is
+//submitted, by using preventDefault() method
+$('#formId').on('submit', (e) => {//onsubmit of form this function will be invoked
+    e.preventDefault();
+
+    socketClient.emit('createMessage', {
+        from: 'User',
+        text: $('#messageId').val()
+    }, () => {
+        //3rd argum of emit(), which is for acknowledgement
+
+    })
+})
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const socketIO = require('socket.io')
 const http = require('http'); //built in node module
 const _ = require('lodash')
-// const message = require('./utils/message')
 //instead we could have use Object destructring technique here i.e-
 const {
     generateMessage
@@ -14,14 +13,10 @@ const port = process.env.PORT || 3000;
 // console.log(__dirname+'/../public');
 
 const publicPath = path.join(__dirname + '/../public')
-// console.log(publicPath);
 
 
 
-
-var app = express(); //It is use to configure the expressjs application
-//note : we dont configure the expressjs by calling the express, rather we configure expressjs appln by
-//caling this variable -> app 
+var app = express(); //It is use to configure the expressjs application note : we dont configure the expressjs by calling the express, rather we configure expressjs appln by caling this variable -> app 
 var server = http.createServer(app);
 //when we call app.listen() method , this internally call this  http.createServer() method
 var socketServer = socketIO(server); //configure server to use 3party module -> socketIO
@@ -29,53 +24,15 @@ var socketServer = socketIO(server); //configure server to use 3party module -> 
 app.use(express.static(publicPath)); //to setup the public folder which have all the frontend static pages
 
 
-//this method let's u register an event listener at server side, soo that @server side we can do some logic/implem
-//when that event happens @client side
-//this 'connection' ->(built-in event) lets us to listen to new connection, (ie when a new client is connected
-//to our server this event will be triggered) and inside the callback fun we write our implemen logic when
-//new client is connected
+//this method let's u register an event listener at server side, soo that @server side we can do some logic/implem when that event happens @client side this 'connection' ->(built-in event) lets us to listen to new connection, (ie when a new client is connected to our server this event will be triggered) and inside the callback fun we write our implemen logic when new client is connected
 socketServer.on('connection', (socketClient) => {
-    //we write our implemen logic when new client is connected
-    //this -> socketClient , is similiar to socket present inside the client side
+    //we write our implemen logic when new client is connected this -> socketClient , is similiar to socket present inside the client side
     console.log('New user is connected!!, Connected to client');
 
-    //emit -> we use emit() inorder to emit/send the event @client and @server side
-    //emit() -> this is not a listener , its an emit
-    //since it is not listener , soo need to specifiy the callback as second argument
-    //in second arugum we specifiy the data which we want to send, bydefault it can be empty if we dont want to
-    //send the data
-
-    /* socketClient.emit('newEmail', {
-        from : "tsabunkar@gmail.com",
-        text :'Hey what is going on',
-        createdAt : 123
-    }) */
-
-    //This is the listener which listens to the createEmail -> custome event create
-    /* socketClient.on('createEmail', (dataSendFromClient) =>{
-        console.log('new email', dataSendFromClient);
-    }); */
-
-
-    //chat application
-    /*  var date = new Date();
-    socketClient.emit('newMessage', {
-        from : "Tejas",
-        text :'Welcome 2 expressMedia',
-        createdAt : (date.getDate())+'/'+(date.getMonth()+1)+'/'+(date.getFullYear())
-    }) */
-
-
-
+    //emit -> we use emit() inorder to emit/send the event @client and @server side emit() -> this is not a listener , its an emit since it is not listener , soo need to specifiy the callback as second argument in second arugum we specifiy the data which we want to send, bydefault it can be empty if we dont want to send the data
 
     var date = new Date();
     //It is to great/welcome all the clients ,who r joining our application 
-    /*  socketClient.emit('newMessage', {
-         from: 'Admin',
-         text: 'welcome to chat app',
-         createdAt: (date.getDate()) + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear())
-     
-     }); */
 
     socketClient.emit('newMessage',
         /*   message.generateMessage('Admin', 'welcome to chat app') */
@@ -85,15 +42,10 @@ socketServer.on('connection', (socketClient) => {
     //It is to say other clients who has already join the chat appln that, a new user has joined
     socketClient.broadcast.emit('newMessage',
         generateMessage('Admin', 'New-user joined chat app')
-        /* {
-            from: 'Admin',
-            text: 'New user joined !!',
-            createdAt: (date.getDate()) + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear())
-        } */
     )
 
     // var date = new Date();
-    socketClient.on('createMessage', (dataSendFromClient) => {
+    socketClient.on('createMessage', (dataSendFromClient, callback) => {
         console.log('new email', dataSendFromClient);
 
         //If the particular client broadcast an event, then this event is recieved to all the other
@@ -101,21 +53,10 @@ socketServer.on('connection', (socketClient) => {
 
         socketServer.emit('newMessage',
             generateMessage(dataSendFromClient.from, dataSendFromClient.text)
-            /*  {
-                from: dataSendFromClient.from,
-                text: dataSendFromClient.text,
-                createdAt: (date.getDate()) + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear())
-            } */
         );
 
-
-        //If the particular client broadcast an event, then this event shld be recieved to all the other
-        //clients but not to the same client who has fired this event
-        /*  socketClient.broadcast.emit('newMessage', {
-             from: dataSendFromClient.from,
-             text: dataSendFromClient.text,
-             createdAt: (date.getDate()) + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear())
-         }) */
+        callback('this message is from server'); //this callback fun will call 3rd argument of socketClient.emit('createMessage', {}, ()=> );
+        //which is written in the client code
     });
 
 
@@ -124,15 +65,6 @@ socketServer.on('connection', (socketClient) => {
         console.log('client has beed disconnected !');
     })
 })
-
-
-
-
-/* 
-app.get('/',(request,response)=> {
-    response.render('index.html', {})
-})//end of get() method
- */
 
 
 server.listen(port, () => {
